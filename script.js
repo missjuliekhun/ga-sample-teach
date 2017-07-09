824f51d4fcc7e84e3fb38b64d7a00f6d07a574c1
Julie Khun <missjuliekhun@gmail.com>
2017-07-09 15:12:09 -0400

    Classroom prototype creating

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // var Organism = Object.create(null);
-var Organism = Object.create(null);
-var Animal = Object.create(Organism);
-var Mammal = Object.create(Animal);
-var Dog = Object.create(Mammal);
-var Spot = Object.create(Dog);
+// var Animal = Object.create(Organism);
+// var Mammal = Object.create(Animal);
+// var Dog = Object.create(Mammal);
+// var Spot = Object.create(Dog);
+//
+// Mammal.hasHair = true;
 
-Mammal.hasHair = true;
+var inClassroom = Object.create(null);
+var Student = Object.create(inClassroom);
+
+
+Student.name = "Edward";
